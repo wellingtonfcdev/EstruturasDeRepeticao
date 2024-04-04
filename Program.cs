7ecc720b883a55c9f0688a11c164b88ee7dd856c
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-04 14:20:30 -0300

    Estrutura de repeticao FOR

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int numero = 5;
+
+Console.WriteLine($"{numero} x 1 = {numero * 1}");
+Console.WriteLine("**************************");
+//For
+for(int contador = 0; contador <= 10; contador++)
+{
+    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+}
