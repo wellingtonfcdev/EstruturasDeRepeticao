9bed9a876b6d285b902f404afb718a6c5b4b0cba
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-04 15:26:43 -0300

    Implementacao laco de repeticao While

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
 ﻿int numero = 5;
 
 Console.WriteLine($"{numero} x 1 = {numero * 1}");
-Console.WriteLine("**************************");
+Console.WriteLine("**************************\n");
 //For
 for(int contador = 0; contador <= 10; contador++)
 {
     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+}
+
+//While
+Console.WriteLine("********* WHILE *********** " + DateTime.Now);
+
+int contWhile = 0;
+while(contWhile <= 10)
+{
+    Console.WriteLine($"{numero} x {contWhile} = {numero * contWhile}");
+    contWhile++;
 }
